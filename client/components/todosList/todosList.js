81a703788bfe0932c9cc6827d27e0320d1520c9b
Viktor Novak <viktor_novak@epam.com>
2016-06-06 23:24:15 +0300

    Meteor tutorial part 9

--- client/components/todosList/todosList.js
+++ client/components/todosList/todosList.js
@@ -45,12 +45,7 @@ class TodosListCtrl {
 
     addTask(newTask) {
         // Insert a task into the collection
-        Tasks.insert({
-            text: newTask,
-            createdAt: new Date,
-            owner: Meteor.userId(),
-            username: Meteor.user().username
-        });
+        Meteor.call('tasks.insert', newTask);
 
         // Clear form
         this.newTask = '';
@@ -58,15 +53,11 @@ class TodosListCtrl {
 
     setChecked(task) {
         // Set the checked property to the opposite of its current value
-        Tasks.update(task._id, {
-            $set: {
-                checked: !task.checked
-            },
-        });
+        Meteor.call('tasks.setChecked', task._id, !task.checked);
     }
 
     removeTask(task) {
-        Tasks.remove(task._id);
+        Meteor.call('tasks.remove', task._id);
     }
 }
 
